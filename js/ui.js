39ae59554fa9ba92b3d226f069778621b80563f5
Owen Campbell-Moore <owencmoore@gmail.com>
2013-04-04 23:30:12 +0100

    Half refactored MLBC stuff to a background process

--- js/ui.js
+++ js/ui.js
@@ -7,9 +7,24 @@ function proceed(mlbcForEncodingAndDecoding) {
   }
 
   function promptDownload(uri) {
-    alert("Please add \".jpg\" to the end of the file name.");
-    var downloadUrl = uri.replace("image/jpeg", "image/octet-stream");
-    top.location.replace(downloadUrl);
+    setTimeout(function() {
+      alert("Please add \".jpg\" to the end of the file name.");
+      var downloadUrl = uri.replace("image/jpeg", "image/octet-stream");
+      top.location.replace(downloadUrl);
+    }, 100); //This isn't call straight away to avoid a chrome bug.
+  }
+
+  function requestEncoding(cover, message, password, callback) {
+    chrome.extension.sendMessage({action: "encode", coverURL: coverURL, message: message, password: password}, function(response) {
+      console.log(response);
+      callback(response.uri)
+    });
+  }
+
+  function requestImageDimensions(coverURL, callback) {
+    chrome.extension.sendMessage({action: "getDimensions", coverURL: coverURL}, function(response) {
+      callback(response.dimensions)
+    });
   }
 
   function encodeImageClicked() {
@@ -17,11 +32,7 @@ function proceed(mlbcForEncodingAndDecoding) {
     var password = document.getElementById("password").value;
     if ((message.length > 0)&&(password.length > 0)&&coverURL) {
       // updateStatus("Recompressing image. Please wait...");
-        getImageDataFromURL(coverURL, 
-          function(data) {
-            encodeData(data, 75, createEncodingDctFunction(message, password, mlbcForEncodingAndDecoding), promptDownload);
-          }
-        );
+        requestEncoding(coverURL, message, password, promptDownload)
     } else {
       alert("Please ensure you have chosen an image and set both the message and the password.");
     }
@@ -34,18 +45,19 @@ function proceed(mlbcForEncodingAndDecoding) {
       f = files[0];
       coverURL = window.webkitURL.createObjectURL(f);
       evt.target.files = [];
-      getImageDimensions(coverURL, function(size) {
+      function imageDimensionsCallback(size) {
         if (size.width % 16 != 0 || size.height % 16 != 0) {
           alert("Error: please select an image with width and height as a multiple of 16 (e.g. 960*720).");
         } else {
           var coefficientCount = (size.width * size.height / 64) - Math.floor(1+5*3*8*mlbcForEncodingAndDecoding.n/mlbcForEncodingAndDecoding.k);
           var maxLength = Math.floor(coefficientCount/8*mlbcForEncodingAndDecoding.k/mlbcForEncodingAndDecoding.n);
-          alert("This image has space for "+maxLength+" characters of hidden message");
+          setTimeout(function() {alert("This image has space for "+maxLength+" characters of hidden message");},100);//This timeout avoids a Chrome bug
           document.getElementById("message").value = document.getElementById("message").value.slice(0, maxLength);
           document.getElementById("message").setAttribute("maxlength", maxLength);
         }
-      });
-      document.getElementById("encoded").src = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+      }
+      requestImageDimensions(coverURL, imageDimensionsCallback);
+      //document.getElementById("encoded").src = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
     } else {
       coverURL = undefined;
     }
